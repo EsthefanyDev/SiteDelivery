c6f30700b1edf5789a97c2eb42bb91aed91411f8
Esthefany L. <esthefany.dev23@gmail.com>
2023-10-31 21:37:46 -0300

    adicionei umas coisas,mas vou tirar depois

--- Arquivos/js/script.js
+++ Arquivos/js/script.js
@@ -1,7 +1,10 @@
-let menu = document.querySelector('#menu-icon');
+/*let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
-}
+}*/
+
+const reoveProductButton = document.getElementsByClassName("remove-product-button")
+console.log(reoveProductButton)
